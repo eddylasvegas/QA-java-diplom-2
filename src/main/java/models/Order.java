f4555e5c56a9e98159f4417f4bbaeea51cae7539
Eldar Gamidov <eldar.gamidov8@yandex.ru>
2025-06-17 16:01:58 +0300

    Добавлено использование @DaTa и @AllArgsConstructor из библиотеки Lombok и обновлен pom.xml

--- src/main/java/models/Order.java
+++ src/main/java/models/Order.java
@@ -1,19 +1,13 @@
 package models;
 
+import lombok.Data;
+import lombok.AllArgsConstructor;
+
 import java.util.List;
 
+@Data
+@AllArgsConstructor
 public class Order {
     private List<String> ingredients;
 
-    public Order(List<String> ingredients) {
-        this.ingredients = ingredients;
-    }
-
-    public List<String> getIngredients() {
-        return ingredients;
-    }
-
-    public void setIngredients(List<String> ingredients) {
-        this.ingredients = ingredients;
-    }
 }
