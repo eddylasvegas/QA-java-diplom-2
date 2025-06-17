9f50534176ffe5fc0195fe785a13aadc457bacbf
Eldar Gamidov <eldar.gamidov8@yandex.ru>
2025-06-17 16:12:28 +0300

    Вынесены URL API в отдельный класс Endpoints

--- src/main/java/api/ApiClient.java
+++ src/main/java/api/ApiClient.java
@@ -7,29 +7,25 @@ import models.User;
 import static io.restassured.RestAssured.given;
 
 public class ApiClient {
-    private static final String AUTH_REGISTER = "/api/auth/register";
-    private static final String AUTH_LOGIN = "/api/auth/login";
-    private static final String AUTH_USER = "/api/auth/user";
-    private static final String ORDERS = "/api/orders";
 
     public Response createUser(User user) {
         return given()
                 .spec(ApiConfig.getDefaultRequestSpec())
                 .body(user)
-                .post(AUTH_REGISTER);
+                .post(Endpoints.AUTH_REGISTER);
     }
 
     public Response loginUser(User user) {
         return given()
                 .spec(ApiConfig.getDefaultRequestSpec())
                 .body(user)
-                .post(AUTH_LOGIN);
+                .post(Endpoints.AUTH_LOGIN);
     }
 
     public Response deleteUser(String token) {
         return given()
                 .spec(ApiConfig.getRequestSpecWithAuth(token))
-                .delete(AUTH_USER);
+                .delete(Endpoints.AUTH_USER);
     }
 
     public Response createOrder(Order order, String token) {
@@ -37,12 +33,12 @@ public class ApiClient {
             return given()
                     .spec(ApiConfig.getRequestSpecWithAuth(token))
                     .body(order)
-                    .post(ORDERS);
+                    .post(Endpoints.ORDERS);
         } else {
             return given()
                     .spec(ApiConfig.getDefaultRequestSpec())
                     .body(order)
-                    .post(ORDERS);
+                    .post(Endpoints.ORDERS);
         }
     }
 }
--- src/main/java/api/ApiConfig.java
+++ src/main/java/api/ApiConfig.java
@@ -5,18 +5,17 @@ import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 
 public class ApiConfig {
-    public static final String BASE_URL = "https://stellarburgers.nomoreparties.site";
 
     public static RequestSpecification getDefaultRequestSpec() {
         return new RequestSpecBuilder()
-                .setBaseUri(BASE_URL)
+                .setBaseUri(Endpoints.BASE_URL)
                 .setContentType(ContentType.JSON)
                 .build();
     }
 
     public static RequestSpecification getRequestSpecWithAuth(String token) {
         return new RequestSpecBuilder()
-                .setBaseUri(BASE_URL)
+                .setBaseUri(Endpoints.BASE_URL)
                 .setContentType(ContentType.JSON)
                 .addHeader("Authorization", token)
                 .build();
